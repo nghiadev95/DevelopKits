5220ee9d6adec12a79909b1f5932b43f6750165b
Nghia Nguyen <quangnghiadev@gmail.com>
2019-12-16 15:50:55 +0700

    init project

---
+++ iOSKits/iOSKits.h
@@ -0,0 +1,19 @@
+//
+//  iOSKits.h
+//  iOSKits
+//
+//  Created by Nghia Nguyen on 12/16/19.
+//  Copyright © 2019 Nghia Nguyen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSKits.
+FOUNDATION_EXPORT double iOSKitsVersionNumber;
+
+//! Project version string for iOSKits.
+FOUNDATION_EXPORT const unsigned char iOSKitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSKits/PublicHeader.h>
+
+
---
+++ iOSKitsDemos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSKitsDemos
+//
+//  Created by Nghia Nguyen on 12/16/19.
+//  Copyright © 2019 Nghia Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
